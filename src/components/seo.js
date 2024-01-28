63800f48f49483a0dad0ac14d8b001b94ded47e5
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-01-28 00:27:09 -0500

    Upload _app.js and _document.js and their dependencies

---
+++ src/components/seo.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+export const Seo = (props) => {
+  const { title } = props;
+
+  const fullTitle = title ? title + ' | PlayPal' : 'PlayPal';
+
+  return (
+    <Head>
+      <title>{fullTitle}</title>
+    </Head>
+  );
+};
+
+Seo.propTypes = {
+  title: PropTypes.string,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import { Seo } from 'src/components/seo';
+// import { usePageView } from 'src/hooks/use-page-view';
+// import { Layout as MarketingLayout } from 'src/layouts/marketing';
+
+const Page = () => {
+  // usePageView();
+
+  return (
+    <>
+      <Seo />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+};
+
+// Page.getLayout = (page) => <MarketingLayout>{page}</MarketingLayout>;
+
+export default Page;
