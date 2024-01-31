fea649721840866bd0a027ce2c89fab05b188f94
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-01-31 15:27:26 -0500

    Add tokens.js for layouts/dashboard/config.js

---
+++ src/locales/tokens.js
@@ -0,0 +1,11 @@
+export const tokens = {
+  nav: {
+    overview: 'nav.overview',
+    account: 'nav.account',
+    room: 'nav.room',
+    roomList: 'nav.roomList',
+    roomCreate: 'nav.roomCreate',
+    calendar: 'nav.calendar',
+    chat: 'nav.chat',
+  },
+};
