0aec1bd7c594b0b31df4397c2f7a125c8f4994b7
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-03-01 00:03:16 -0500

    #31 Display member's sports can play on member card/list.

--- src/sections/rooms/member-card.js
+++ src/sections/rooms/member-card.js
@@ -3,6 +3,7 @@ import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Chip from "@mui/material/Chip";
 
 export const MemberCard = (props) => {
   const { member } = props;
@@ -34,21 +35,23 @@ export const MemberCard = (props) => {
           {/*</Typography>*/}
         </div>
       </Stack>
-      {/*<Stack*/}
-      {/*  alignItems="center"*/}
-      {/*  direction="row"*/}
-      {/*  flexWrap="wrap"*/}
-      {/*  spacing={1}*/}
-      {/*  sx={{ mt: 2 }}*/}
-      {/*>*/}
-      {/*  {(member.sports_you_can_play).map((sport) => (*/}
-      {/*    <Chip*/}
-      {/*      key={sport}*/}
-      {/*      label={sport}*/}
-      {/*      size="small"*/}
-      {/*    />*/}
-      {/*  ))}*/}
-      {/*</Stack>*/}
+      {member.sports_you_can_play.length > 0 ? (
+        <Stack
+          alignItems="center"
+          direction="row"
+          flexWrap="wrap"
+          spacing={1}
+          sx={{ mt: 2 }}
+        >
+          {member.sports_you_can_play.map((sport) => (
+            <Chip
+              key={sport.name}
+              label={sport.name}
+              size="small"
+            />
+          ))}
+        </Stack>
+      ) : null}
     </Box>
   );
 };
